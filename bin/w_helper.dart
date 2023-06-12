64b6636ff4caa11625fe9b5552a0b457ccfa8185
HACI DAYI <hdayi@hotmail.com>
2023-06-12 06:36:14 +0300

    second occurance

--- bin/w_helper.dart
+++ bin/w_helper.dart
@@ -2,6 +2,17 @@ import 'dart:convert';
 import 'dart:io';
 import 'package:turkish/turkish.dart';
 
+extension SecondOccurenceOfSubstring on String {
+  int secondIndexOf(String stringToFind) {
+    if (indexOf(stringToFind) == -1) return -1;
+    return indexOf(stringToFind, indexOf(stringToFind) + 1);
+  }
+
+  bool hasSecondOccurence(String stringToFind) {
+    return secondIndexOf(stringToFind) != -1;
+  }
+}
+
 void main(List<String> arguments) {
   File("./data/all.txt")
       .openRead()
@@ -11,17 +22,18 @@ void main(List<String> arguments) {
     (kelime) {
       if (kelime.length == 5 && !kelime.contains(" ")) {
         var k = kelime.toLowerCaseTr();
-        if (!k.contains("k") &&
-            !k.contains("a") &&
-            !k.contains("e") &&
-            !k.contains("m") &&
-            !k.contains("t") &&
-            !k.contains("o") &&
-            !k.contains("z") &&
-            !k.contains("u") &&
-            k.contains("r") &&
-            k.indexOf("r") != 1 &&
-            k.indexOf("l") == 3) {
+        if ( //
+            // !k.contains("k") &&
+            //     !k.contains("a") &&
+            //     !k.contains("e") &&
+            //     !k.contains("m") &&
+            //     !k.contains("t") &&
+            //     !k.contains("o") &&
+            //     !k.contains("z") &&
+            //     !k.contains("u") &&
+            k.contains("a") && k.indexOf("a") == 1 && k.secondIndexOf("a") == 4
+            //
+            ) {
           print(k);
         }
       }
